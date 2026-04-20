02937a34e610c4f9148781dafe268e2c82dfb9ce
jonny0608 <527jlanier@frhsd.com>
2026-04-20 10:36:40 -0400

    initial commit: add basic structure for to-do list application with task management functionality

---
+++ app.js
@@ -0,0 +1,43 @@
+let tasks = []; // Empty array to hold tasks
+
+document.getElementById("addTaskBtn").addEventListener('click', function () {
+    // get the value of the input field
+    let taskInput = document.getElementById("taskInput").value;
+    // check if input field is empty
+    if (taskInput) {
+        //add the task to the array
+        tasks.push(taskInput);
+        // clear the input field 
+        document.getElementById("taskInput").value = "";
+        //update task list display
+        displayTasks();
+    }
+});
+
+function displayTasks() {
+    //select the task list from HTMl
+    let taskList = document.getElementById("taskList");
+    //clear existing tasks
+    taskList.innerHTML = '';
+    //loop through each task in the array and create a list item for each
+    tasks.forEach((task, index) => {
+        //Create a new list element for each task (li)
+        let li = document.createElement('li');
+
+        //add styling 
+        li.classList.add(
+            'list-group-item',
+            'd-flex',
+            'justify-content-between',
+            'align-items-center'
+        );
+        //set the inner HTML of the list item to the task text
+        li.innerHTML = `${task} <button class='btn btn-success btn-sm' onclick='removeTask(${index})'>✓</button>`;
+        //append the list item to the task list in HTML
+        taskList.appendChild(li);
+    })
+
+
+    //append the list item to the task list in HTML
+
+}
